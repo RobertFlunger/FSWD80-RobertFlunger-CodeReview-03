7f3bc340f4d98a8a86aacd4c8aaf7021884703b2
RobertFlunger <55085544+RobertFlunger@users.noreply.github.com>
2019-09-27 13:18:02 +0200

    Index.html and javascript functions added

---
+++ js/script.js
@@ -0,0 +1,66 @@
+
+var customerName = document.getElementById('name').value;
+
+/*regular expression*/
+
+function getName() {
+	var letters = /^$|^[A-z\s*]+$/;
+	customerName = document.getElementById('name').value;
+	if(letters.test(customerName)) {
+		console.log('Clean');
+	} else {
+		alert('Numbers and special characters are not valid as an input here. Please input letters only.');
+		document.getElementById('name').value = '';
+		customerName = '';
+	}
+}
+
+function getAge() {
+	customerAge = document.getElementById('age').value;
+	if(isNaN(parseInt(customerAge))) {
+		alert('Please input your age as a number here.');
+		document.getElementById('age').value = '';
+	}
+}
+
+function getHP() {
+	horsePower = document.getElementById('horsepower').value;
+	if(isNaN(parseInt(horsePower))) {
+		alert('Please input the horse power of your vehicle as a number here.');
+		document.getElementById('horsepower').value = '';
+	}
+}
+
+document.getElementById('name').addEventListener('focusout', getName);
+document.getElementById('age').addEventListener('focusout', getAge);
+document.getElementById('horsepower').addEventListener('focusout', getHP);
+
+var customerCountry = document.getElementById('countrySelect').value;
+
+function calculateInsurance() {
+	var insurance
+	if (customerCountry == 'Austria') {
+		insurance = horsePower * 100 / customerAge + 50;
+	} else if (customerCountry == 'Hungary') {
+		insurance = horsePower * 120 / customerAge + 100;
+	} else {
+		insurance = horsePower * 150 / (customerAge + 3) + 50;
+	}
+	document.getElementById('calculationOutput').innerHTML = customerName + ", your insurance costs " + insurance.toFixed(2) + "€."
+}
+
+document.getElementById('calculateButton').addEventListener('click', function() {
+																		calculateInsurance(horsePower, customerAge, customerCountry) })
+
+
+
+
+
+
+
+
+
+
+
+
+
